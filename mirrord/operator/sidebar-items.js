e668202afbd03c5e75d866756514a758121f1996
meowjesty <43983236+meowjesty@users.noreply.github.com>
2023-09-07 17:14:09 +0000

    Deploying to gh-pages from @ meowjesty/mirrord@735b9f6eb2afdace81ee372abf8efd87e3742ba5 🚀

--- mirrord/operator/sidebar-items.js
+++ mirrord/operator/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["get_status_api","operator_command","operator_setup","operator_status"]};
+window.SIDEBAR_ITEMS = {"fn":["get_last_version","get_status_api","operator_command","operator_setup","operator_status"],"struct":["OperatorVersionResponse"]};

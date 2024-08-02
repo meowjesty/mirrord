ab94a51bcd13683fc767a3bf8ea85e49969bca3a
meowjesty <meowjesty_nyan@outlook.com>
2024-08-02 11:14:57 -0300

    no execv

--- mirrord/layer/src/exec_hooks/hooks.rs
+++ mirrord/layer/src/exec_hooks/hooks.rs
@@ -143,7 +143,7 @@ pub(crate) unsafe extern "C" fn execve_detour(
 pub(crate) unsafe fn enable_exec_hooks(hook_manager: &mut HookManager) {
     // TODO(alex) [high]: `execv` by itself is fine.
     // #[cfg(not(target_os = "macos"))]
-    replace!(hook_manager, "execv", execv_detour, FnExecv, FN_EXECV);
+    // replace!(hook_manager, "execv", execv_detour, FnExecv, FN_EXECV);
 
     replace!(hook_manager, "execve", execve_detour, FnExecve, FN_EXECVE);
 }
